35e541d2a96f0c43b162fd9adb0c47e1e42d3cff
firefresh <demetriodowbnac@gmail.com>
2020-06-14 21:49:57 +0000

    update funcional

---
+++ home/coder/.bash_history
@@ -0,0 +1,52 @@
+ls
+cd .config/
+ls
+cd code-server/
+cat config.y
+cat config.yml
+htop
+service code-server restart
+htop
+cd /usr/lib/code-server/
+ls
+cd code-server 
+ls
+cd code-server 
+cd bin/
+ls
+cd code-server 
+cd ..
+ls
+ls -a
+cat package.json 
+ls
+cd lib/
+ls
+cd vscode/
+ls
+ls -a
+cd resources/
+ls
+cd ~
+ls
+cd -a
+ls -a
+ls
+ls -a
+cd .config/
+ls
+cd code-server/
+ls
+cd config.yaml
+cat config.yaml
+htop
+python
+htop
+htop
+htop
+sudo apt install python pip git -y
+apt update
+sudo apt update
+sudo apt install python pip git -y
+sudo apt install python python-pip git -y
+htop
--- run_code-server.sh
+++ run_code-server.sh
@@ -7,9 +7,9 @@ echo "Code Server"
 echo "http://$ip:8443"
 
 security_group=$(ec2-metadata -s | cut -d " " -f 2);
-aws ec2 authorize-security-group-ingress --group-name $security_group --protocol tcp --port 8443 --cidr 0.0.0.0/0
+#aws ec2 authorize-security-group-ingress --group-name $security_group --protocol tcp --port 8443 --cidr 0.0.0.0/0
 
-docker run -d -p 8443:8080 -v "$PWD:/home/coder/project" -u "$(id -u):$(id -g)" codercom/code-server:3.4.1
+docker run -d -p 8443:8080 -p 443:443 -v "$PWD/home:/home" -u "$(id -u):$(id -g)" codercom/code-server:3.4.1
 
 #version="1.32.0-310"
 #wget https://github.com/codercom/code-server/releases/download/$version/code-server-$version-linux-x64.tar.gz
