ca4fbf81c8ef349ee22fb680ae727f47d092b2c6
f1refresh <53023423+firefresh@users.noreply.github.com>
2021-02-07 19:05:32 -0300

    Update run_code-server.sh

--- run_code-server.sh
+++ run_code-server.sh
@@ -1,21 +1,12 @@
 # 
 
-ip=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
+ip=0.0.0.0
 complete_ip="$ip:8443"
 
 echo "Code Server"
 echo "http://$ip:8443"
 
-security_group=$(ec2-metadata -s | cut -d " " -f 2);
-#aws ec2 authorize-security-group-ingress --group-name $security_group --protocol tcp --port 8443 --cidr 0.0.0.0/0
-
 docker run -d -p 8443:8080 -p 443:443 -v "$PWD/home:/home" -u "$(id -u):$(id -g)" codercom/code-server:3.4.1
 
-#version="1.32.0-310"
-#wget https://github.com/codercom/code-server/releases/download/$version/code-server-$version-linux-x64.tar.gz
-#tar -xvzf code-server-$version-linux-x64.tar.gz
-#cd code-server-$version-linux-x64
-#chmod +x code-server
-#./code-server -p 8443
 
 
